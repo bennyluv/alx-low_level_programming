7029b729c185d277cb1da00b33385bf61e0069e6
bennyluv <onyinyebenny2222@gmail.com>
2023-02-24 16:08:19 -0800

    task 0

--- 0x02-functions_nested_loops/Positive_or_negative.c
+++ 0x02-functions_nested_loops/Positive_or_negative.c
@@ -1,16 +1,30 @@
-#include "main.h"
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-        int i;
+int n;
 
-        i = 98;
-        positive_or_negative(i);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-        return (0);
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
